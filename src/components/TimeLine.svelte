f32225b290863e8a09bd769d989aef29382b6875
Victor Casta <victorarroyavecastaneda@gmail.com>
2023-09-11 22:46:57 -0500

    [ADD] styles for components

---
+++ src/components/TimeLine.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Card from './Card.svelte'
+</script>
+
+<style>
+    .TimeLine {
+        padding: 4em 0 0 0;
+    }
+</style>
+
+<div class="TimeLine">
+    <div class="Timeline-container">
+        <Card />
+    </div>
+</div>
--- src/containers/app.svelte
+++ src/containers/app.svelte
@@ -1,11 +1,28 @@
+<script>
+    import Header from '../components/Header.svelte'
+    import Main from '../components/Main.svelte'
+    import TimeLine from '../components/TimeLine.svelte'
+    import Sidebar from '../components/Sidebar.svelte'
+</script>
+
 
 <style>
     :global(body) {
         background-color: #F2EFE9;
         color: #333333;
+        font-family: 'Nunito Sans', sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    :global(h1, h2, h3) {
+        margin: 0;
+        padding: 0;
     }
 </style>
 
-<div class="main">
-    <h1>Hello Casta</h1>
-</div>
+<Header />
+    <Main>
+        <TimeLine />
+        <Sidebar/>
+    </Main>
